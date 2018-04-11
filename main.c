a918129e4fbd9fa9730f29fd0ebce636ddb5c84e
Clausolas <38120586+Clausolas@users.noreply.github.com>
2018-04-11 11:20:21 +0900

    ㅁㅁ

--- main.c
+++ main.c
@@ -8,3 +8,26 @@ return 0;
 
 
 }
+
+#include <stdio.h>
+
+
+
+int main(){
+	int a, b;
+	scanf_s("&d", a);
+	scanf_s("$d", b);
+	mainasu(a,b);
+	return 0;
+
+}
+
+int mainasu(int a, int b)
+{
+	int sum = 0;
+	sum = a + b;
+	printf("&d", sum);
+
+
+
+}
